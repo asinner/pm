e2e94ef15aef7b76b415a0c35bc6df6b29aec326
Andrew Sinner <andrewsinner@gmail.com>
2015-02-04 20:52:58 -0800

    Add project spec

--- spec/requests/projects/getting_a_project_spec.rb
+++ spec/requests/projects/getting_a_project_spec.rb
@@ -5,6 +5,21 @@ RSpec.describe 'Getting a project', type: :request do
   let(:company) {user.companies.first}
   let(:token) {FactoryGirl.create(:token, user: user)}
   let(:project) {FactoryGirl.create(:project, company: company)}
+  let(:unauthorized_company) {FactoryGirl.create(:company)}
+  let(:unauthorized_project) {FactoryGirl.create(:project, company: unauthorized_company)}
+
+  
+  context 'that does not exist' do
+    it 'returns a 404' do
+      expect {
+        get "/api/projects/0", nil, {
+          'X-Auth-Token' => token.string,
+          'Accept' => 'application/json',
+          'Content-Type' => 'application/json'
+        }
+      }.to raise_error(ActiveRecord::RecordNotFound)
+    end
+  end
   
   context 'that a user is authorized for' do
     context 'with a company with a valid subscription' do
@@ -18,6 +33,34 @@ RSpec.describe 'Getting a project', type: :request do
         expect(response.status).to eq(200)
         expect(response.content_type).to eq(Mime::JSON)
       end
-    end 
+    end
+    
+    context 'with a company with an invalid subscription' do
+      before {company.update(subscription_status: 'cancel')}
+      
+      it 'returns a 402' do
+        get "/api/projects/#{project.id}", nil, {
+          'X-Auth-Token' => token.string,
+          'Accept' => 'application/json',
+          'Content-Type' => 'application/json'
+        }
+        
+        expect(response.status).to eq(402)
+        expect(response.content_type).to eq(Mime::JSON)
+      end
+    end
+  end
+  
+  context 'that a user is not authorized for' do
+    it 'returns a 403' do
+      get "/api/projects/#{unauthorized_project.id}", nil, {
+        'X-Auth-Token' => token.string,
+        'Accept' => 'application/json',
+        'Content-Type' => 'application/json'
+      }
+      
+      expect(response.status).to eq(403)
+      expect(response.content_type).to eq(Mime::JSON)
+    end
   end
 end
