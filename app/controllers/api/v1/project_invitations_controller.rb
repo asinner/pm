500fbd03e0f313bcae136186cef61f4618fa1029
Andrew Sinner <andrewsinner@gmail.com>
2015-06-10 14:36:30 -0700

    Random spacing issues

--- app/controllers/api/v1/project_invitations_controller.rb
+++ app/controllers/api/v1/project_invitations_controller.rb
@@ -1,17 +1,17 @@
 class Api::V1::ProjectInvitationsController < ApplicationController
   before_action :authenticate_user
-  
+
   def create
     project = Project.find(params[:project_id])
     user = User.find_by(email: params[:recipient])
-    
+
     invitation = Invitation.new(invitable: project, recipient: params[:recipient])
     authorize invitation
-    
+
     return render status: 402, json: {msg: 'Please renew your subscription'} if project.company.inactive?
     return render status: 422, json: {msg: 'User is already on this project'} if user && project.team.includes?(user)
     invitation.key = TokenService.create_for(Invitation, :key)
-    
+
     if invitation.save
       ProjectInvitationWorker.perform_async(invitation.id)
       render status: 201, json: invitation
